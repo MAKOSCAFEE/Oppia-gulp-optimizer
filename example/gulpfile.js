2bbe127bccbfa27bd3d1a018fd93c5c4c4d041bb
MAKOSCAFEE <barnabasmakonda@gmail.com>
2015-10-23 04:29:17 +0300

    Added ability to receive argments

--- example/gulpfile.js
+++ example/gulpfile.js
@@ -1,12 +1,36 @@
 var gulp = require('gulp');
-// Find index.js file
+// Finds index.js file
 var gae = require('../');
+var path = require('path');
+// Argrments which will be passed in start.sh
+var argv = require('yargs')
+    .usage('Usage: $0 --pythoncmd [python command]' +
+      '--appengine [Path to app engine]' +
+      '--clear_datastore [clear all data storage]' +
+      '--enable_sendmail [send emails]')
+    .demand(['pythoncmd', 'appengine'])
+    .argv;
+var serveCmd = argv.pythoncmd + ' ' + argv.appengine + '/dev_appserver.py';
+var params = {
+  admin_host: '0.0.0.0',
+  admin_port: 8000,
+  host: '0.0.0.0',
+  port: 8181
+};
+
+if (argv.clear_datastore) {
+  params.clear_datastore = true;
+}
+
+if (argv.enable_sendmail) {
+  params.enable_sendmail = argv.enable_sendmail;
+}
 
 gulp.task('gulp-serve', function() {
   gulp.src('app/app.yaml')
     .pipe(gae('dev_appserver.py', [], {
       admin_host: '0.0.0.0',
-      admin_port: 8001,
+      admin_port: 8000,
       host: '0.0.0.0',
       port: 8181
     }));
