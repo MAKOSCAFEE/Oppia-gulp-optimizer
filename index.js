29fa518254b9636d70876284ffdf23ef74af7745
MAKOSCAFEE <barnabasmakonda@gmail.com>
2015-11-02 23:11:01 +0300

    Customized more for oppia usage

--- index.js
+++ index.js
@@ -10,16 +10,11 @@ var File = gutil.File;
 var defaultStartAction = 'dev_appserver.py';
 
 module.exports = function(action, args, params) {
-  action = action || 'dev_appserver.py';
+  action = action || defaultStartAction;
   args = args || [];
   params = params || {};
 
   var proc;
-
-  if (['dev_appserver.py', 'appcfg.py'].indexOf(action) == -1) {
-    throw new PluginError('oppia-gulp', 'Invalid action ' + action +
-      '. Supported actions are dev_appserver.py and appcfg.py');
-  }
 
   var parseParams = function(params) {
     var p = [];
@@ -46,7 +41,7 @@ module.exports = function(action, args, params) {
   };
 
   var stopScript = function() {
-    gutil.log('[oppia-gulp]', 'stopping script');
+    gutil.log('[gulp-gae]', 'stopping script');
     proc && proc.kill('SIGHUP');
     proc = null;
   };
@@ -55,11 +50,8 @@ module.exports = function(action, args, params) {
     var appYamlPath = path.dirname(file.path);
     var shouldWait = false;
 
-    if (action == 'dev_appserver.py') {
+    if (action) {
       args = [appYamlPath].concat(args);
-    } else if (action == 'appcfg.py') {
-      args = args.concat([appYamlPath]);
-      shouldWait = true;
     }
 
     runScript(action, args, params, cb);
