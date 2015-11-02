05fca3d4f1b47075d3a8fef2456d03479d12b3f8
MAKOSCAFEE <barnabasmakonda@gmail.com>
2015-11-02 23:15:39 +0300

    removed args for demostration purpose

--- example/gulpfile.js
+++ example/gulpfile.js
@@ -2,21 +2,6 @@ var gulp = require('gulp');
 // Finds index.js file
 var gae = require('../');
 var path = require('path');
-// Argrments which will be passed in start.sh
-var argv = require('yargs')
-    .usage('Usage: $0 --pythoncmd [python command]' +
-      '--appengine [Path to app engine]' +
-      '--clear_datastore [clear all data storage]' +
-      '--enable_sendmail [send emails]')
-    .demand(['pythoncmd', 'appengine'])
-    .argv;
-var serveCmd = argv.pythoncmd + ' ' + argv.appengine + '/dev_appserver.py';
-var params = {
-  admin_host: '0.0.0.0',
-  admin_port: 8000,
-  host: '0.0.0.0',
-  port: 8181
-};
 
 if (argv.clear_datastore) {
   params.clear_datastore = true;
